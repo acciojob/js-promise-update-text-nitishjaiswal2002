efcee8514da2e96ff7eeb2b21c2ce3cc5c355194
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-10-31 12:59:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code heHre. If required.
 function delayedHello() {
-	return new promise ((resolve,reject) =>{
+	return new Promise ((resolve,reject) =>{
 		setTimeout(() =>{
 			resolve('Hello, world!');
 		},1000);
