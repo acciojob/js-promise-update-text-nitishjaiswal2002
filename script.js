8fd35a7f4faa0bad61672f361c1e3f17175e98b1
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-10-31 12:54:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+//your JS code heHre. If required.
+function delayedHello() {
+	return new promise ((resolve,reject) =>{
+		setTimeout(() =>{
+			resolve('Hello, world!');
+		},1000);
+	});
+}
+
+async function updateOutput(){
+	const outputElement =document.getElementById('output');
+	 try{
+		 const message =await delayedHello();
+		 outputElement.textContent = message;
+	 }
+	catch(error){
+		outputElement.textContent ='Error occurred: '+ error;
+	}
+}
+updateOutput();
